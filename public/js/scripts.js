0e237da974d55457391ad549ac3f281b374d0d62
Josep Conesa Botia <35197649+websJC@users.noreply.github.com>
2019-08-02 19:52:44 +0200

    Add files via upload
    cambios en función para copiar enlace

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,10 +2,19 @@ const formulario = document.querySelector('#agregar-url');
 formulario.addEventListener('submit', async e => {
     e.preventDefault();
     const urlOriginal = document.querySelector('#urlOriginal').value;
-    const respuesta = await fetch(e.target.action, {method: e.target.method,headers: {'Accept' : 'application/json','Content-Type' : 'application/json'},body: JSON.stringify({urlOriginal})});
+    const respuesta = await fetch(e.target.action, {
+        method: e.target.method,
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({urlOriginal})
+    } );
     const resultado = await respuesta.json();
     const alertas = document.querySelector('.mensaje-url');
-    if(alertas){document.querySelector('.mensaje-url').remove();};
+    if(alertas){
+        document.querySelector('.mensaje-url').remove();
+    };
     if(resultado.codigo === 201){
         const mensaje = document.createElement('div');
         mensaje.classList.add('mensaje-url');
@@ -13,26 +22,20 @@ formulario.addEventListener('submit', async e => {
         console.log("pathApp: "+ pathApp);
         var urlAcortada = `${pathApp}${resultado.url}`;
         console.log("urlAcortada: "+ urlAcortada);
-        mensaje.innerHTML = `<p>Se ha acortado correctamente la url, visita <a href="${resultado.url}" target="_blank" rel="noopener noreferrer" title="Ir a la URL acortada">Enlace</a>`+
-                            `<br><input type="text" readonly placeholder="${urlAcortada}" value="${urlAcortada}" id="inputResultadoUrlAcortada" name="inputResultadoUrlAcortada">`+
-                            `<button title="Copiar url" id="botonCopiarUrl"><i class="far fa-copy"><i></button></p>`;
-       
+        mensaje.innerHTML = `<p id="pUrl">Se ha acortado correctamente la url, visita <a id="enlaceUrl" href="${resultado.url}" target="_blank" rel="noopener noreferrer" title="Ir a la URL acortada">Enlace</a>`;
         const contenedor = document.querySelector('main');
         contenedor.appendChild(mensaje);
-        document.getElementById('botonCopiarUrl').onclick = function(){
-                let textoACopiar = document.getElementById('inputResultadoUrlAcortada').select();
-                document.execCommand("copy");
-                console.log("Se ha copiado el texto");
-        };
+        afegirCaixa(urlAcortada);
+        
     } else {
         const mensaje = document.createElement('div');
         mensaje.classList.add('mensaje-url','error');
         mensaje.innerHTML = `<p>Se ha producido un error ${resultado.error} </p>`;
         const contenedor = document.querySelector('main');
-        contenedor.appendChild(mensaje);
+      
     }
 
-})
+});
 const urlParams = new URLSearchParams(window.location.search);
 if(urlParams.has('error')){
     const mensaje = document.createElement('div');
@@ -41,3 +44,16 @@ if(urlParams.has('error')){
     const contenedor = document.querySelector('main');
     contenedor.appendChild(mensaje);
 };
+
+function afegirCaixa(urlAcortada){
+    // let shortLink = document.getElementById('inputResultadoUrlAcortada');
+    let mensajeBoton = `<br><input type="text" readonly placeholder="${urlAcortada}" value="${urlAcortada}" id="inputResultadoUrlAcortada" name="inputResultadoUrlAcortada">`+
+                            `<button title="Copiar url" id="botonCopiarUrl">Copiar Enlace <i class="far fa-copy"><i></button></p>`;
+    document.getElementById('pUrl').innerHTML+=mensajeBoton;
+
+    document.getElementById('botonCopiarUrl').onclick = function(){
+        let textoACopiar = document.getElementById('inputResultadoUrlAcortada').select();
+        document.execCommand("copy");
+        console.log("Se ha copiado el texto");
+};
+}
